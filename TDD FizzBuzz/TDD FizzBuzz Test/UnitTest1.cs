10c3955483ac606645fee1955a1364ff03c776c1
sahobau.dasilva <sahobau.dasilva@PC0120>
2017-12-27 14:01:26 +0100

    FizzBuzzALT

---
+++ TDD FizzBuzz/TDD FizzBuzz Test/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_FizzBuzz;
+
+namespace TDD_FizzBuzz_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldReturn1IfInputIs1()
+        {
+            Assert.AreEqual("1", FizzBuzz.Get(1));
+        }
+        [TestMethod]
+        public void ShouldReturn2IfInputIs2()
+        {
+            Assert.AreEqual("2", FizzBuzz.Get(2));
+        }
+        [TestMethod]
+        public void ShouldReturnFizzIfInputIs3()
+        {
+            Assert.AreEqual("Fizz", FizzBuzz.Get(3));
+        }
+        [TestMethod]
+        public void ShouldReturn4IfInputIs4()
+        {
+            Assert.AreEqual("4", FizzBuzz.Get(4));
+        }
+        [TestMethod]
+        public void ShouldReturnBuzzIfInputIs5()
+        {
+            Assert.AreEqual("Buzz", FizzBuzz.Get(5));
+        }
+        [TestMethod]
+        public void ShouldReturnFizzIfInputIs6()
+        {
+            Assert.AreEqual("Fizz", FizzBuzz.Get(6));
+        }
+        [TestMethod]
+        public void ShouldReturnBuzzIfInputIs10()
+        {
+            Assert.AreEqual("Buzz", FizzBuzz.Get(10));
+        }
+        [TestMethod]
+        public void ShouldReturnFizzBuzzIfInputIs15()
+        {
+            Assert.AreEqual("FizzBuzz", FizzBuzz.Get(15));
+        }
+        [TestMethod]
+        public void ShouldReturnFizzBuzzIfInputIs30()
+        {
+            Assert.AreEqual("FizzBuzz", FizzBuzz.Get(30));
+        }
+        [TestMethod]
+        public void ShouldReturn12IfInputIs12()
+        {
+            Assert.AreEqual("12", FizzBuzz.Get(1,2));
+        }
+        [TestMethod]
+        public void ShouldReturn12FizzIfInputIs123()
+        {
+            Assert.AreEqual("12Fizz", FizzBuzz.Get(1, 3));
+        }
+        [TestMethod]
+        public void ShouldReturn4IfInputIs42()
+        {
+            Assert.AreEqual("4", FizzBuzz.Get(4, 2));
+        }
+        [TestMethod]
+        public void ShouldReturn12Fizz4BuzzFizz78FizzIfInputIs123456789()
+        {
+            Assert.AreEqual("12Fizz4BuzzFizz78Fizz", FizzBuzz.Get(1, 9));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidImputExeption))]
+        public void ShouldReturnInvalidImputExeptionIfInputsIsNegative()
+        {
+            FizzBuzz.Get(-1);
+        }
+    }
+}
---
+++ TDD FizzBuzz/TDD FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public static string Get(int min, int max=0)
+        {
+            if (min <0 || max <0)
+            {
+                throw new InvalidImputExeption();
+            }
+            int counter = max-min>0?max-min+1 :1;
+            StringBuilder stg = new StringBuilder();
+            while (counter>0)
+            {
+
+                stg.Append(GetFizzBuzzValue(min));
+                counter--;
+                min++;
+               
+            }
+            return stg.ToString();
+           
+        }
+
+        private static String GetFizzBuzzValue(int nb) {
+
+            if (nb % 3 == 0 && nb % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (nb % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (nb % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+               return nb.ToString();
+            }
+        }
+    }
+}
