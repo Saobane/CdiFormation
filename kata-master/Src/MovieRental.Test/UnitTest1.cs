61a3241076613ea2360ba6167f334315babe014a
sahobau.dasilva <sahobau.dasilva@PC0120>
2018-01-16 17:26:35 +0100

    Kata Man

---
+++ kata-master/Src/MovieRental.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieRental.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestStatement()
+        {
+            Movie movie = new Movie("Transformer", Movie.REGULAR);
+
+            Rental rental = new Rental(movie, 3);
+
+            Customer customer = new Customer("jpartogi");
+            customer.AddRental(rental);
+
+            String statement = customer.Statement();
+            StringAssert.Contains("Transformer", statement);
+        }
+    }
+}
